316f044448afbff95a58db656709ea3077116331
Sylvain Huang <105045701+Kusanagies@users.noreply.github.com>
2026-06-03 15:10:23 +0200

    Ajout du code qui permet de générer un graphe de corrélation entre la distance entre un domicile à une station de transport

---
+++ correlation.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data {
+    pd.read_csv('/data/dvf.csv')
+    pd.read_csv('/data/gtfs-stops-france-export-2026-01-13')
+}
